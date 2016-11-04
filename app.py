b6bb0b5a3e51224cf573726a4c1f437d7d1a6448
Alex Wang <alex3763@gmail.com>
2016-11-04 10:39:30 -0400

    expand our horizons

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
+#!/usr/bin/env python3
+from math import *
 import sys
 
 print("Let's play the game!")
 while True:
 	try:
 		x = str(input("Zero or Not Zero: "))
-		if eval(x) == 0:
+		if not x:
+			print("It's not hard, you just have to know how to do it.")
+			continue
+		if x.lower() == "zero":
 			print("That one is ZERO.")
-		else:
+			continue
+		try:	
+			if eval(x) == 0 :
+				print("That one is ZERO.")
+			else:
+				print("That one is NOTZERO.")
+		except NameError:
 			print("That one is NOTZERO.")
 	except KeyboardInterrupt:
 		print("")
 		print("Doesthatmakesense.")
-		sys.exit()
+		break
