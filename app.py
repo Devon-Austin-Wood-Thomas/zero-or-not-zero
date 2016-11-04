624f770b4cf85557f7da7ec3846495d7538093d2
Kevin Geng <gengkev@gmail.com>
2016-11-04 10:52:39 -0400

    improve

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
+
 from math import *
 import sys
+
+try:
+    input = raw_input
+except NameError:
+    pass
+
 
 print("Let's play the game!")
 while True:
 	try:
-		x = str(input("Zero or Not Zero: "))
+		x = input("Zero or Not Zero: ")
 		if not x:
 			print("It's not hard, you just have to know how to do it.")
 			continue
-		if x.lower() == "zero":
+		if x.lower() == "zero" or x.lower() == "alex wang":
 			print("That one is ZERO.")
 			continue
-		try:	
-			if eval(x) == 0 :
+		try:
+			if eval(x) == 0:
 				print("That one is ZERO.")
 			else:
 				print("That one is NOTZERO.")
-		except (NameError, SyntaxError) :
+		except (NameError, SyntaxError):
 			print("That one is NOTZERO.")
 	except KeyboardInterrupt:
 		print("")
 		print("Doesthatmakesense.")
 		break
+
