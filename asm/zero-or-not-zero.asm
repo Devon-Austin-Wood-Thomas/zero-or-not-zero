328dc8392eeb8722d789d494d4e21b0880dbff4b
98WuG <gwu02251250@gmail.com>
2018-07-24 16:17:46 +0000

    Documentation

--- asm/zero-or-not-zero.asm
+++ asm/zero-or-not-zero.asm
@@ -25,43 +25,46 @@ section .bss
 section .text
 
 	_start:
-
 	; Print greeting to stdout
-	mov	edx, greetingLen
-	mov	ecx, greeting
-	mov	ebx, 1
-	mov	eax, 4
-	int	0x80
+	mov	edx, greetingLen	; Arg 3: Length of string
+	mov	ecx, greeting		; Arg 2: Location of string
+	mov	ebx, 1				; Arg 1: File descriptor
+	mov	eax, 4				; Syscall: write(2)
+	int	0x80				; Kernel interrupt 0x80
 
 	; Read user input and store it in ecx register
-	mov	edx, 8
-	mov	ecx, num
-	mov	ebx, 2
-	mov	eax, 3
-	int	0x80
+	mov	edx, 8		; Arg 3: Size of user input
+	mov	ecx, num	; Arg 2: Store user input in ecx
+	mov	ebx, 2		; Arg 1: idk it's like something
+	mov	eax, 3		; Syscall: read i guess? i just learned assembly today
+	int	0x80		; Kernel interrupt 0x80
 
 	; If ecx == 0 setZero else setNotZero
-	cmp	byte[ecx], '0'
-	je	setZero
-	jmp	setNotZero
+	cmp	byte[ecx], '0'	; Compare character value of user input to ascii char 0
+	je	setZero			; Jump to setZero if equal
+	jmp	setNotZero		; Jump to setNotZero
 
+	; Exit the program
 	exit:
 	mov	ebx, 0		; Arg 1: status
 	mov	eax, 1		; Syscall: 1 = exit(3)
 	int	0x80		; Kernel interrupt 0x80
 
+	; Set edx and ecx registers to print zero message
 	setZero:
 	mov	edx, zeroLen
 	mov	ecx, zero
 	jmp	print
 
+	; Set edx and ecx registers to print notzero message
 	setNotZero:
 	mov	edx, notzeroLen
 	mov	ecx, notzero
 	jmp	print
 
+	; Print whatever is in edx and ecx registers to stdout
 	print:
 	mov	ebx, 1		; Arg 1: File descriptor (1 = stdout)
 	mov	eax, 4		; Syscall: 4 = write(2)
-	int	0x80
-	call	exit
+	int	0x80		; Kernel interrupt 0x80
+	call	exit	; Exit program
